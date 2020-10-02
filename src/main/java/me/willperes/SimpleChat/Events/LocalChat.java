c360b08d4c5824eef7b141944be439f48ec216ff
Kavatch <alex@pluoi.com>
2020-10-02 23:43:45 +0200

    - reformatted a lot of the code to make it more easy to understand - removed config methode infavor of the default methode from spigot

--- src/main/java/me/willperes/SimpleChat/Events/LocalChat.java
+++ src/main/java/me/willperes/SimpleChat/Events/LocalChat.java
@@ -10,78 +10,82 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class LocalChat implements Listener {
 
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent e) {
 
-        sendMessage(e.getMessage(),e.getPlayer().getLocation(),e.getPlayer());
+        sendMessage(e.getMessage(), e.getPlayer().getLocation(), e.getPlayer());
         e.setCancelled(true);
 
     }
 
     private void sendMessage(String message, Location loc, Player whoSent) {
-
-        if(!(whoSent.hasPermission("simplechat.local.use"))) {
+        if (!(whoSent.hasPermission("simplechat.local.use"))) {
             whoSent.sendMessage(ChatColor.RED + "You don't have permission to do this.");
             return;
         }
 
-        String playerGroupPrefix = null;
-        int i;
-
-        if(SimpleChatMain.chat.getPrimaryGroup(whoSent) != null) {
-
-            String[] allGroups = SimpleChatMain.chat.getGroups();
-
-            for(i = 0 ; i < allGroups.length ; i++) {
-                if(allGroups[i].equals(SimpleChatMain.chat.getPrimaryGroup(whoSent))) {
-                    playerGroupPrefix = SimpleChatMain.chat.getGroupPrefix(whoSent.getWorld(),allGroups[i]);
-                }
-            }
-
-        }
+        String playerGroupPrefix = getPlayerGroupPrefix(whoSent);
 
         // Radius of local message (can be edited at the config.yml file)
-        int radius = SimpleChatMain.plugin.getConfig().getInt("RadiusNumber");
+        int radius = SimpleChatMain.getPlugin().getConfig().getInt("RadiusNumber");
 
         /*
         Verify if the player that will receive the message is within the sender's radius. If the player that will receive
         the message is within the radius, he will receive the message. In case there is no one within the radius, the
         sender will receive a message that no one is close to hear him
-         */
-        ArrayList<Player> players = new ArrayList<>(Bukkit.getServer().getOnlinePlayers());
-        if(players.size() == 1) {
-            whoSent.sendMessage(ChatColor.translateAlternateColorCodes('&', "&e[L] "
-                    + playerGroupPrefix + "&f" + whoSent.getDisplayName() + "&e: " + message));
+        */
+        List<Player> players = new ArrayList<>(Bukkit.getServer().getOnlinePlayers());
+
+        if (players.size() == 1) {
+            whoSent.sendMessage(
+                    ChatColor.translateAlternateColorCodes('&', "&e[L] " + playerGroupPrefix + "&f" + whoSent.getDisplayName() + "&e: " + message)
+            );
             whoSent.sendMessage(ChatColor.YELLOW + "There is no one close to hear you...");
             return;
         }
-        for(i = 0 ; i < players.size() ; i++) {
-            if(players.get(i).getLocation().distanceSquared(loc) < Math.pow(radius,2)) {
 
-                if(players.get(i).equals(whoSent)) {
-                    // player equal player
-                }else {
-                    players.get(i).sendMessage(ChatColor.translateAlternateColorCodes('&', "&e[L] "
-                            + playerGroupPrefix + "&f" + whoSent.getDisplayName() + "&e: " + message));
-                    whoSent.sendMessage(ChatColor.translateAlternateColorCodes('&', "&e[L] "
-                            + playerGroupPrefix + "&f" + whoSent.getDisplayName() + "&e: " + message));
-                    return;
-                }
+        boolean hasBeenSend = false;
 
-            // There is no one in the player radius.
-            }else{
-
-                whoSent.sendMessage(ChatColor.translateAlternateColorCodes('&', "&e[L] "
-                        + playerGroupPrefix + "&f" + whoSent.getDisplayName() + "&e: " + message));
-                whoSent.sendMessage(ChatColor.YELLOW + "There is no one close to hear you...");
-                return;
-
+        for (Player player : players) {
+            if (player.getLocation().distance(loc) > radius) {
+                continue;
             }
+
+            if (player.equals(whoSent)) {
+                continue;
+            }
+
+            player.sendMessage(
+                    ChatColor.translateAlternateColorCodes('&', "&e[L] " + playerGroupPrefix + "&f" + whoSent.getDisplayName() + "&e: " + message)
+            );
+
+            hasBeenSend = true;
+        }
+
+        whoSent.sendMessage(
+                ChatColor.translateAlternateColorCodes('&', "&e[L] " + playerGroupPrefix + "&f" + whoSent.getDisplayName() + "&e: " + message)
+        );
+
+        if (!hasBeenSend) {
+            whoSent.sendMessage(ChatColor.YELLOW + "There is no one close to hear you...");
         }
 
     }
 
+    public static String getPlayerGroupPrefix(Player whoSent) {
+        if (SimpleChatMain.chat.getPrimaryGroup(whoSent) != null) {
+            String[] allGroups = SimpleChatMain.chat.getGroups();
+            for (String group : allGroups) {
+                if (!group.equals(SimpleChatMain.chat.getPrimaryGroup(whoSent))) {
+                    continue;
+                }
+                return SimpleChatMain.chat.getGroupPrefix(whoSent.getWorld(), group);
+            }
+        }
+        return null;
+    }
 }
